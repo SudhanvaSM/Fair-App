805c4c36a99f601340d8fef6275e148c8ed1c4cb
Sudhanva S M <sudhanva.madhusudhan@gmail.com>
2026-06-24 22:06:42 +0530

    feat: add delete all groups functionality and update API URL

--- frontend/app/(tabs)/groups.tsx
+++ frontend/app/(tabs)/groups.tsx
@@ -1,15 +1,18 @@
-import { View, StyleSheet, ScrollView, Alert } from "react-native";
+import { View, StyleSheet, ScrollView, Alert, Pressable } from "react-native";
 import { useCallback, useState } from "react";
-import { router, useFocusEffect } from "expo-router";
+import { router, Stack, useFocusEffect } from "expo-router";
 
 import { GroupSummary } from "@/types/item";
 
 import { createGroupWithMembers, getDetailedGroup, getGroupSummary, getGroupsWithMembers } from "@/src/services/group.service";
+import { resetAppData } from "@/src/services/user.services";
 
 import useScrollToTop from "../hooks/useScrollToTop";
 
 import AddBlock from "@/components/AddBlock";
 import Groups from "@/components/Groups";
+import { Menu } from "react-native-paper";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function GroupsScreen() {
 
@@ -64,44 +67,111 @@ export default function GroupsScreen() {
 		});
 	};
 
-	return (
-		<ScrollView
-			style={styles.scrollView}
-			contentContainerStyle={{ paddingBottom: 40 }}
-			showsVerticalScrollIndicator={false}
-			keyboardShouldPersistTaps="handled"
-			ref={scrollRef}
-		>
-			<View style={{ marginTop: 10, paddingHorizontal: 10, justifyContent: "center" }}>
-				{groupState.map((group) => {
-					return (
-						<View key={group.id}style={{ alignItems: "center" }}>
-						<Groups
-						title={group.name}
-						people={group.members.length}
-						total={group.totalExpenses}
-						onPress={() => openGroupDetails(group.id || 0)}
-						variant={((group.id ?? 0) % 2 !== 0) ? "1" : "2"}
-						/>
-						</View>
-					);
-				})}
-				</View>
+	const [visible, setVisible] = useState(false);
+	const openMenu = () => setVisible(true);
+	const closeMenu = () => setVisible(false);
+	
+	const deleteAllGroups = () => {
+		closeMenu();
+		Alert.alert (
+			"Confirm Action",
+			"Are you sure you want to delete all groups?\nThis will RESET app data and delete existing splits.",
+			[
+				{
+					text: "Cancel",
+					style: "cancel",
+				},
+				{
+					text: "Delete",
+					style: "destructive",
+					onPress: async() => {
+						try {
+							resetAppData();
+							setGroupState([]);
+						}
+						catch (e) {
+							console.error("Remove all splits failed: ", e);
+						}
+					}
+				}
+			],
+			{
+				cancelable: true,
+			}
+		);
+	}
 
-				<View style={{ alignItems: "center" }}>
-					<AddBlock
-						showInput={showInput}
-						setShowInput={setShowInput}
-						newName={newItemName}
-						setNewName={setNewItemName}
-						members={members}
-						setMembers={setMembers}
-						memberInput={memberInput}
-						setMemberInput={setMemberInput}
-						onAdd={addItem}
-					/>
-				</View>
-		</ScrollView>
+	return (
+		<>
+		<Stack.Screen
+			options={{
+				headerRight: () => (
+					<Menu
+					contentStyle={{ marginTop: 35, borderRadius: 20, backgroundColor: "#334155" }}
+					visible={visible}
+					onDismiss={closeMenu}
+					anchor={
+						<Pressable 
+							style={{ justifyContent: "center", alignItems: "center", paddingRight: 20 }}
+							hitSlop={10} 
+							onPress={openMenu}
+						>
+							<MaterialCommunityIcons
+								name={"dots-vertical"}
+								color={"white"}
+								size={24}
+							/>
+						</Pressable>
+					}
+				>
+				<Menu.Item
+					hitSlop={10}
+					onPress={() => deleteAllGroups()}
+					title="Delete All Groups"
+					titleStyle={{ color: "red" }}
+				/>
+				</Menu>
+				)
+			}}
+		/>
+			<ScrollView
+				style={styles.scrollView}
+				contentContainerStyle={{ paddingBottom: 40 }}
+				showsVerticalScrollIndicator={false}
+				keyboardShouldPersistTaps="handled"
+				ref={scrollRef}
+			>
+				<View style={{ marginTop: 10, paddingHorizontal: 10, justifyContent: "center" }}>
+					{groupState.map((group) => {
+						return (
+							<View key={group.id}style={{ alignItems: "center" }}>
+							<Groups
+							title={group.name}
+							people={group.members.length}
+							total={group.totalExpenses}
+							onPress={() => openGroupDetails(group.id || 0)}
+							variant={((group.id ?? 0) % 2 !== 0) ? "1" : "2"}
+							/>
+							</View>
+						);
+					})}
+					</View>
+
+					<View style={{ alignItems: "center" }}>
+						<AddBlock
+							showInput={showInput}
+							setShowInput={setShowInput}
+							newName={newItemName}
+							setNewName={setNewItemName}
+							members={members}
+							setMembers={setMembers}
+							memberInput={memberInput}
+							setMemberInput={setMemberInput}
+							onAdd={addItem}
+						/>
+					</View>
+			</ScrollView>
+		</>
 	);
 }
 
